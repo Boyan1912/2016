7bbbb95bca9f3035ccdaba0f9585d432aa0f2a4c
Boyan Denkov <buya1@mail.bg>
2016-04-28 23:17:55 +0300

    Task 1 done

---
+++ CoBuilder BG Ltd Tasks/01_Numbers_List/NumbersList/Numbers.cs
@@ -0,0 +1,61 @@
+﻿namespace NumbersList
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public class Numbers
+    {
+        static void Main(string[] args)
+        {
+            IList<int> numbers = GetRandomNumbers(100, -100, 100);
+
+            Console.WriteLine("Initial list:");
+            PrintList(numbers);
+
+            RemoveNegativeValues(numbers);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Negative valuues cleared:");
+            PrintList(numbers);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static void RemoveNegativeValues(IList<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int number = numbers[i];
+                if (number <= 0)
+                {
+                    numbers.RemoveAt(i);
+                    RemoveNegativeValues(numbers);
+                }
+            }
+        }
+        
+        static void PrintList(IList<int> numbers)
+        {
+            if (numbers.Count < 1)
+            {
+                Console.Write("The list is empty!");
+            }
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static IList<int> GetRandomNumbers(int count = 50, int min = int.MinValue, int max = int.MaxValue)
+        {
+            List<int> list = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int num = rnd.Next(min, max);
+                list.Add(num);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ CoBuilder BG Ltd Tasks/01_Numbers_List/NumbersListTests/NumbersTests.cs
@@ -0,0 +1,73 @@
+﻿namespace NumbersList.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+
+    [TestClass()]
+    public class NumbersTests
+    {
+        [TestMethod()]
+        public void TestIfOnlyPositiveNumbersRemainWithMixedData()
+        {
+            IList<int> numbers = Numbers.GetRandomNumbers();
+            Numbers.RemoveNegativeValues(numbers);
+            foreach (int num in numbers)
+            {
+                Assert.IsTrue(num > 0);
+            }
+        }
+
+        [TestMethod()]
+        public void TestIfOnlyPositiveNumbersRemainWithPositiveNumbersOnly()
+        {
+            IList<int> numbers = Numbers.GetRandomNumbers(min: 1);
+            int length = numbers.Count;
+            Numbers.RemoveNegativeValues(numbers);
+
+            Assert.AreEqual(length, numbers.Count);
+            foreach (int num in numbers)
+            {
+                Assert.IsTrue(num > 0);
+            }
+        }
+
+        [TestMethod()]
+        public void TestIfListIsEmptyWithNegativeNumbersOnly()
+        {
+            IList<int> numbers = Numbers.GetRandomNumbers(max: 1);
+            Numbers.RemoveNegativeValues(numbers);
+
+            Assert.AreEqual(numbers.Count, 0);
+        }
+
+        [TestMethod()]
+        public void TestIfListIsEmptyWithEmptyList()
+        {
+            IList<int> numbers = new List<int>();
+            Numbers.RemoveNegativeValues(numbers);
+
+            Assert.AreEqual(numbers.Count, 0);
+        }
+
+        [TestMethod()]
+        public void TestIfListIsEmptyWithOnlyZerosList()
+        {
+            IList<int> numbers = Numbers.GetRandomNumbers(min: 0, max: 1);
+            Numbers.RemoveNegativeValues(numbers);
+
+            Assert.AreEqual(numbers.Count, 0);
+        }
+
+        [TestMethod()]
+        public void TestIfOnlyOnesRemainFromBitsArray()
+        {
+            IList<int> numbers = Numbers.GetRandomNumbers(min: 0, max: 2);
+            Numbers.RemoveNegativeValues(numbers);
+
+            foreach (int num in numbers)
+            {
+                Assert.IsTrue(num == 1);
+            }
+        }
+    }
+}
